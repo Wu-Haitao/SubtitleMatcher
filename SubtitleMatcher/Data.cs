7f4dd6e8aed981f7a494e9b58f4d66554fd64511
Wu-Haitao <wuhaitao99.st@gmail.com>
2023-10-10 22:38:26 +0800

    添加项目文件。

---
+++ SubtitleMatcher/Data.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitleMatcher
+{
+    class Data
+    {
+        public static ObservableCollection<ListItem> leftList = new();
+        public static ObservableCollection<ListItem> rightList = new();
+        public class ListItem
+        {
+            string path;
+            public ListItem(string path) { this.path = path; }
+            public string GetPath() { return path; }
+            public override string ToString()
+            {
+                return Path.GetFileName(path);
+            }
+        }
+        public static void RemoveListItemWithIndex(bool isLeft, int index)
+        {
+            if (index == -1) return;
+            if (isLeft)
+            {
+                leftList.RemoveAt(index);
+            }
+            else
+            {
+                rightList.RemoveAt(index);
+            }
+        }
+        public static void MoveUpListItemWithIndex(bool isLeft, int index)
+        {
+            if (index == -1) return;
+            if (index == 0) return;
+            if (isLeft)
+            {
+                ListItem listItem = leftList[index];
+                leftList[index] = leftList[index - 1];
+                leftList[index - 1] = listItem;
+            }
+            else
+            {
+                ListItem listItem = rightList[index];
+                rightList[index] = rightList[index - 1];
+                rightList[index - 1] = listItem;
+            }
+        }
+        public static void MoveDownListItemWithIndex(bool isLeft, int index)
+        {
+            if (index == -1) return;
+            if (isLeft)
+            {
+                if (index == leftList.Count - 1) return;
+                ListItem listItem = leftList[index];
+                leftList[index] = leftList[index + 1];
+                leftList[index + 1] = listItem;
+            }
+            else
+            {
+                if (index == rightList.Count - 1) return;
+                ListItem listItem = rightList[index];
+                rightList[index] = rightList[index + 1];
+                rightList[index + 1] = listItem;
+            }
+        }
+        public static void SortList(bool isLeft)
+        {
+            if (isLeft)
+            {
+                List<ListItem> sortedList = leftList.OrderBy(item => item.ToString()).ToList();
+                leftList.Clear();
+                foreach (ListItem item in sortedList)
+                {
+                    leftList.Add(item);
+                }
+            }
+            else
+            {
+                List<ListItem> sortedList = rightList.OrderBy(item => item.ToString()).ToList();
+                rightList.Clear();
+                foreach (ListItem item in sortedList)
+                {
+                    rightList.Add(item);
+                }
+            }
+        }
+        public static void ApplyFileNames(string addon)
+        {
+            List<string> paths = leftList.Select(item => item.GetPath()).ToList();
+            List<string> newNames = rightList.Select(item => item.ToString()).ToList();
+
+            for (int i = 0; i < paths.Count; i++)
+            {
+                try
+                {
+                    string sourcePath = paths[i];
+                    string newName = newNames[i];
+                    int addonIndex = newName.LastIndexOf('.');
+                    if (addonIndex == -1) return;
+                    string extension = Path.GetExtension(sourcePath);
+                    newName = newName.Remove(addonIndex + 1);
+                    newName = newName.Insert(addonIndex + 1, addon + extension);
+                    string newFilePath = Path.Combine(Path.GetDirectoryName(sourcePath), newName);
+                    File.Move(sourcePath, newFilePath);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+        public static void ClearList(bool isLeft)
+        {
+            if (isLeft)
+            {
+                leftList.Clear();
+            }
+            else
+            {
+                rightList.Clear();
+            }
+        }
+    }
+}
---
+++ SubtitleMatcher/MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SubtitleMatcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeBinding();
+            InitializeEvent();
+            DataContext = this;
+        }
+        private void InitializeBinding()
+        {
+            LeftList.ItemsSource = Data.leftList;
+            RightList.ItemsSource = Data.rightList;
+        }
+        private void InitializeEvent()
+        {
+            LeftList.Drop += List_Drop;
+            RightList.Drop += List_Drop;
+            LeftDel.Click += Del_Button_Click;
+            RightDel.Click += Del_Button_Click;
+            LeftUp.Click += Up_Button_Click;
+            RightUp.Click += Up_Button_Click;
+            LeftDown.Click += Down_Button_Click;
+            RightDown.Click += Down_Button_Click;
+            LeftSort.Click += Sort_Button_Click;
+            RightSort.Click += Sort_Button_Click;
+            LeftClr.Click += Clear_Button_Click;
+            RightClr.Click += Clear_Button_Click;
+        }
+        private void List_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                switch (((ListView)sender).Name) {
+                    case "LeftList":
+                        foreach (string file in files)
+                        {
+                            Data.leftList.Add(new Data.ListItem(file));
+                        }
+                        break;
+                    case "RightList":
+                        foreach (string file in files)
+                        {
+                            Data.rightList.Add(new Data.ListItem(file));
+                        }
+                        break;
+                    default: break;
+                }
+            }
+        }
+        private void Del_Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Name)
+            {
+                case "LeftDel":
+                    Data.RemoveListItemWithIndex(true, LeftList.SelectedIndex);
+                    break;
+                case "RightDel":
+                    Data.RemoveListItemWithIndex(false, RightList.SelectedIndex);
+                    break;
+                default: break;
+            }
+        }
+        private void Up_Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Name)
+            {
+                case "LeftUp":
+                    int leftIndex = LeftList.SelectedIndex;
+                    Data.MoveUpListItemWithIndex(true, LeftList.SelectedIndex);
+                    LeftList.SelectedIndex = Math.Max(-1, leftIndex - 1);
+                    break;
+                case "RightUp":
+                    int rightIndex = RightList.SelectedIndex;
+                    Data.MoveUpListItemWithIndex(false, RightList.SelectedIndex);
+                    RightList.SelectedIndex = Math.Max(-1, rightIndex - 1);
+                    break;
+                default: break;
+            }
+        }
+        private void Down_Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Name)
+            {
+                case "LeftDown":
+                    int leftIndex = LeftList.SelectedIndex;
+                    Data.MoveDownListItemWithIndex(true, LeftList.SelectedIndex);
+                    LeftList.SelectedIndex = leftIndex + 1;
+                    break;
+                case "RightDown":
+                    int rightIndex = RightList.SelectedIndex;
+                    Data.MoveDownListItemWithIndex(false, RightList.SelectedIndex);
+                    RightList.SelectedIndex = rightIndex + 1;
+                    break;
+                default: break;
+            }
+        }
+        private void Sort_Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Name)
+            {
+                case "LeftSort":
+                    Data.SortList(true);
+                    LeftList.SelectedIndex = -1;
+                    break;
+                case "RightSort":
+                    Data.SortList(false);
+                    RightList.SelectedIndex = -1;
+                    break;
+                default: break;
+            }
+        }
+        private void Clear_Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Name)
+            {
+                case "LeftClr":
+                    Data.ClearList(true);
+                    LeftList.SelectedIndex = -1;
+                    break;
+                case "RightClr":
+                    Data.ClearList(false);
+                    RightList.SelectedIndex = -1;
+                    break;
+                default: break;
+            }
+        }
+        private void ExecButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Data.leftList.Count != Data.rightList.Count)
+            {
+                MessageBox.Show("左右项目数量不一致！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            Data.ApplyFileNames(Addon.Text);
+        }
+    }
+}
